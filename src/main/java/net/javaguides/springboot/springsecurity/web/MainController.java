f0d535c9ce8756c95a06f922034abe47a656adc6
rajasci <vsrvenkatareddy@gmail.com>
2021-07-07 17:40:34 +0530

    merged with new index, login and reg pages

--- src/main/java/net/javaguides/springboot/springsecurity/web/MainController.java
+++ src/main/java/net/javaguides/springboot/springsecurity/web/MainController.java
@@ -1,11 +1,13 @@
 package net.javaguides.springboot.springsecurity.web;
 import net.javaguides.springboot.springsecurity.model.*;
+import net.javaguides.springboot.springsecurity.web.dto.EmployeeRegistrationDto;
 
 import java.util.Collection;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class MainController {
@@ -15,6 +17,11 @@ public class MainController {
 	@GetMapping("/i")
     public String root(Model model) {
         return "i";
+    }
+	
+	@ModelAttribute("employee")
+    public EmployeeRegistrationDto userRegistrationDto() {
+        return new EmployeeRegistrationDto();
     }
 //	@GetMapping("/")
 //    public String index1(Model model) {
